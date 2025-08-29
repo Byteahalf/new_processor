1df10b4e0125b44c83f33370e3f309e98e092b0b
lost <byteandahalf@outlook.com>
2025-08-29 12:53:19 +0100

    -

--- xiangshan/run_docker.ps1
+++ xiangshan/run_docker.ps1
@@ -1,4 +1,8 @@
-docker run --rm -it \
-    -v ./:/home/builder/workspace/xiangshan \
-    -w /home/builder/workspace/xiangshan \
-    scala-sbt-min:11 bash
+docker run --rm -it `
+    -v ${PWD}:/home/builder/workspace `
+    -v ${PWD}/NEMU:/opt/NEMU `
+    -v ${PWD}/nexus-am:/opt/nexus-am `
+    -e NOOP_HOME=/home/builder/workspace/xiangshan `
+    -e NEMU_HOME=/opt/NEMU `
+    -e AM_HOME=/opt/nexus-am `
+    scala-dev-env bash
